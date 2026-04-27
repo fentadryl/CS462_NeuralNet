1b1e66a94b8a3ea31a03f236f72cb132a9cce729
fentadryl <brolfe@my.ccsu.edu>
2026-04-27 15:56:39 -0400

    Update Util.java
    fixed formatting, updated sigmoidDeriv to work more efficently

--- src/Util.java
+++ src/Util.java
@@ -1,39 +1,44 @@
 package myneuralnet;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Util {
-	  public static double sigmoid(double in){
-	    return 1 / (1 + Math.exp(-in));
-	  }
-	  public static double sigmoidDeriv(double in){
-	      double sigmoid = Util.sigmoid(in);
-	      return sigmoid * (1 - in);
+	public static double sigmoid(double in) {
+		return 1 / (1 + Math.exp(-in));
+	}
+	  
+	public static double sigmoidDeriv(double out) {
+		return out * (1 - out);
+	}
+	  
+	public static double getGradient(double output) {
+		// This takes the "guess" and tells you how steep the hill is at that point
+		return output * (1 - output);
+	}
+	  
+	  
+	/** Assumes array args are same length */
+	public static Double meanSquareLoss(List<Double> correctAnswers, List<Double> predictedAnswers) {
+		double sumSquare = 0;
+	    for (int i = 0; i < correctAnswers.size(); i++) {
+	    	double error = correctAnswers.get(i) - predictedAnswers.get(i);
+	    	sumSquare += (error * error);
 	    }
-	    /** Assumes array args are same length */
-	    public static Double meanSquareLoss(List<Double> correctAnswers, List<Double> predictedAnswers){
-	      double sumSquare = 0;
-	      for (int i = 0; i < correctAnswers.size(); i++){
-	        double error = correctAnswers.get(i) - predictedAnswers.get(i);
-		sumSquare += (error * error);
-	      }
-	      return sumSquare / (correctAnswers.size());   
+	    return sumSquare / (correctAnswers.size());   
 	}
 	    
-	    public static double normalize(double val, double min, double max) {
-	        return (val - min) / (max - min);
-	    }
+	public static double normalize(double val, double min, double max) {
+		return (val - min) / (max - min);
+	}
 	    
-	    public static List<List<Double>> getNormalizedData(List<List<Integer>> raw, double minW, double maxW, double minH, double maxH) {
-	        List<List<Double>> normalizedData = new ArrayList<>();
-	        for (List<Integer> row : raw) {
-	            double nW = normalize(row.get(0), minW, maxW);
-	            double nH = normalize(row.get(1), minH, maxH);
-	            normalizedData.add(Arrays.asList(nW, nH));
-	        }
-	        return normalizedData;
+	public static List<List<Double>> getNormalizedData(List<List<Integer>> raw, double minW, double maxW, double minH, double maxH) {
+		List<List<Double>> normalizedData = new ArrayList<>();
+	    for (List<Integer> row : raw) {
+	    	double nW = normalize(row.get(0), minW, maxW);
+	        double nH = normalize(row.get(1), minH, maxH);
+	        normalizedData.add(Arrays.asList(nW, nH));
 	    }
+	    return normalizedData;
+	}
 }
